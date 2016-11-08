84cd61e1d833b62de5a152081c37366dfeba6e66
Stefano Vozza <svozza@gmail.com>
2016-11-08 23:36:23 +0000

    add more future functions

--- talk2/utils.js
+++ talk2/utils.js
@@ -1,5 +1,9 @@
-const {Maybe, Just, Nothing, toMaybe, add} = require('sanctuary');
-const {curry, map, prop, compose, chain, find, reduce, toUpper} = require('ramda');
+const {Right, Left, Just, Nothing, toMaybe, add} = require('sanctuary');
+const {curry, map, prop, compose, chain, find, reduce, toUpper, split} = require('ramda');
+const Future = require('fluture');
+const futurize = require('futurize').futurize(Future);
+const fs = require('fs');
+const request = require('request');
 
 // safeProp :: String -> {String: a} -> Maybe a
 const safeProp = curry(compose(toMaybe, prop));
@@ -35,8 +39,8 @@ const fromMaybe = curry((def, m) => m.isJust ? m.value : def);
 fromMaybe(2, Just(1)); // => 1
 fromMaybe(2, Nothing()); // => 2
 
-// safeFind :: (a -> Boolean) -> [a] -> Maybe a
-const safeFind = curry(compose(toMaybe, find));
+// findById :: (Integer -> Boolean) -> [{String: a}] -> Maybe a
+const findById = curry((id, xs) => toMaybe(find(x => x.id === id, xs)));
 
 //safePath :: [String] -> {String: a} -> Maybe a
 const safePath = curry((path, obj) => reduce((maybe, prop) => {
@@ -58,6 +62,57 @@ if(obj && obj.a && obj.a.b) { // <= does not compose!
 }
 
 // code the happy path instead
-compose(fromMaybe(10), map(add(1)), chain(safePath(['a', 'b', 'c'])), safeFind(x => x.id === 1))(xs);
+compose(fromMaybe(10), map(add(1)), chain(safePath(['a', 'b', 'c'])), findById(1))(xs);
 // => 10
+
+// encaseEither :: (a -> a) -> a -> Either Error a
+const encaseEither = curry((f, x) => {
+  try {
+      return Right(f(x));
+  } catch (e) {
+    return Left(e);
+  }
+});
+
+encaseEither(JSON.parse, '{"a": 1}');
+// Right({a: 1})
+encaseEither(JSON.parse, '{a: 1}');
+// Left(Error(SyntaxError: Unexpected token a in JSON at position 1))
+
+// parseDate :: String -> Either Error Date
+const parseDate = dateStr => {
+    const date = new Date(dateStr);
+    return Number.isNaN(date.getTime()) ? Left(new Error('Invalid Date')) : Right(date)
+};
+
+const json = '{"date": "2014-4-9"}';
+compose(chain(parseDate), map(prop('date')), encaseEither(JSON.parse))(json);
+// => Right(Date('Wed Apr 09 2014 00:00:00 GMT+0100 (BST)'))
+
+// readFile :: String -> Future Error String
+const readFile = file => Future((reject, resolve) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+        if(err) return reject(err);
+        return resolve(data);
+    })
+});
+
+// describe computation
+const authorF = compose(map(prop('author')), map(JSON.parse), readFile)('../package.json');
+
+// execute computation
+authorF.fork(console.error, console.log);
+// => Stefano Vozza
+
+// httpGet :: String -> Future Error Object
+const httpGet = url => Future((reject, resolve) => {
+    request({url, json: true}, (err, res, body) => {
+        if(err) return reject(err);
+        return resolve(body);
+    });
+});
+
+
+
+
 
